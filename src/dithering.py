079213df15a53ad5d656f0c820a39943dd329622
pananananas <101266606+pananananas@users.noreply.github.com>
2023-12-27 23:00:32 +0100

    added quantization to the pipeline

--- src/dithering.py
+++ src/dithering.py
@@ -6,17 +6,29 @@ def ordered_dithering(image, dithering_matrix = 255 * np.array([[0, 8, 2, 10],
                                                                 [12, 4, 14, 6],
                                                                 [3, 11, 1, 9],
                                                                 [15, 7, 13, 5]]) / 16):
-    # Convert the image to grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
     # Get the dimensions of the image
-    height, width = gray_image.shape
+    height, width, _ = image.shape
 
     # Iterate over each pixel in the image
     for i in range(height):
         for j in range(width):
-            old_pixel = gray_image[i, j]
+            old_pixel = image[i, j]
             new_pixel = dithering_matrix[i % 4, j % 4]
-            gray_image[i, j] = 255 if old_pixel > new_pixel else 0
+            image[i, j] = [255 if old_pixel[k] > new_pixel else 0 for k in range(3)]
 
-    return gray_image
+    return image
+
+
+# change the bits of the image to the given number of bits
+def quantization(image, bits):
+    # Get the dimensions of the image
+    height, width, _ = image.shape
+
+    # Iterate over each pixel in the image
+    for i in range(height):
+        for j in range(width):
+            old_pixel = image[i, j]
+            new_pixel = [int((old_pixel[k] / 255) * ((2 ** bits) - 1)) for k in range(3)]
+            image[i, j] = [int((new_pixel[k] / ((2 ** bits) - 1)) * 255) for k in range(3)]
+
+    return image
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 from pykuwahara import kuwahara
+from dithering import ordered_dithering
+from dithering import quantization
 
 import cv2
 import numpy as np
@@ -14,7 +16,11 @@ if image is None:
 image = cv2.resize(image, (1024, 1024))
 
 
-filtered_image = kuwahara(image, method='gaussian', radius=5)    
+filtered_image = quantization(image, 3)
+
+filtered_image = kuwahara(filtered_image, method='gaussian', radius=5)    
+
+# filtered_image = ordered_dithering(filtered_image)
 
 
 # Display the original and filtered image
@@ -23,6 +29,8 @@ cv2.imshow('Kuwahara Filtered Image', filtered_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+file_name = image_path.split('/')[-1].split('.')[0]
+file_format = image_path.split('/')[-1].split('.')[1]
 
 # Save the filtered image
-cv2.imwrite('result/filtered_image.jpg', filtered_image)
+cv2.imwrite('result/' + file_name + '_kuwahara.' + file_format, filtered_image)
